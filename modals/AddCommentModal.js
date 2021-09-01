c72d40e31f46653306b7ad9ad1c4e29a9cca9661
Jblack017 <blackhamjoshua@comcast.net>
2021-08-31 21:30:48 -0600

    adds add comment modal and kiddo comment styling

--- modals/AddCommentModal.js
+++ modals/AddCommentModal.js
@@ -1,12 +1,132 @@
 import React from "react";
-import { SafeAreaView, Text, Modal, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  Modal,
+  Pressable,
+  TextInput,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useSelector, useDispatch } from "react-redux";
 
-export default function LoadingModal({ modalVisible }) {
+export default function AddCommentModal({ modalVisible, setModalVisible }) {
+  const [comment, onChangeComment] = React.useState("");
+  const [commentDate, setCommentDate] = React.useState(new Date());
+  const [datePicker, showDatePicker] = React.useState(false);
+
+  const dispatch = useDispatch();
+  const kiddos = useSelector(state => state);
+
+  const [month, day, year] = [
+    commentDate.getMonth() + 1,
+    commentDate.getDate(),
+    commentDate.getFullYear(),
+  ];
+
+  //Need to send Post with CommentDate format of yyy-mm-dd
+  const kiddoUrl = "https://darndest-be.herokuapp.com/kids";
+  const kiddoComment = {
+    // kiddoId: kiddo.id,
+    comment: comment,
+    commentDate: `${year + "-" + month + "-" + day}`,
+  };
+  // const options = {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify(kiddo),
+  // };
+
+  // const addKiddoToDB = kiddo => {
+  //   if (kiddo.name.length == 0) {
+  //     Alert.alert(
+  //       "Kiddo name and Birthday required.\nPlease check and try again."
+  //     );
+  //   } else {
+  //     fetch(kiddoUrl, options)
+  //       .then(response => response.json())
+  //       .then(kiddo => {
+  //         dispatch({ type: "ADD_KIDDO", newKiddos: [...kiddos, kiddo] });
+  //       })
+  //       .then(setModalVisible(false));
+  //   }
+  // };
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || commentDate;
+    showDatePicker(Platform.OS === "ios");
+    setCommentDate(currentDate);
+  };
+
   return (
-    <Modal animationType='fade' transparent={false} visible={modalVisible}>
+    <Modal
+      animationType='slide'
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        setModalVisible(!modalVisible);
+      }}
+    >
       <SafeAreaView style={styles.centeredView}>
         <SafeAreaView style={styles.modalView}>
-          <Text>Loading</Text>
+          <Text
+            style={{
+              fontSize: 45,
+              margin: 5,
+              fontFamily: "kiddo-font",
+              borderBottomWidth: 2,
+              borderColor: "white",
+            }}
+          >
+            Add Comment
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeComment}
+            value={comment}
+            autoCorrect={false}
+            placeholder='"Add Kiddo Comment Here"'
+          />
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 10,
+            }}
+            onPress={() => showDatePicker(!datePicker)}
+          >
+            <Text style={{ fontFamily: "FatFace-font", fontSize: 16 }}>
+              Quoted Date:
+            </Text>
+            <TextInput
+              editable={false}
+              style={styles.birthdayInput}
+              value={kiddoComment.commentDate}
+              onChangeText={onChange}
+            />
+          </Pressable>
+          {datePicker && (
+            <DateTimePicker
+              testID='dateTimePicker'
+              value={commentDate}
+              mode='date'
+              display='spinner'
+              onChange={onChange}
+            />
+          )}
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            // onPress={() => addKiddoToDB(kiddo)}
+          >
+            <Text style={styles.textStyle}>Add Comment</Text>
+          </Pressable>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(false)}
+          >
+            <Text style={{ ...styles.textStyle, color: "red" }}>Go Back</Text>
+          </Pressable>
         </SafeAreaView>
       </SafeAreaView>
     </Modal>
@@ -17,15 +137,15 @@ const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    marginVertical: 70,
+    marginHorizontal: 35,
   },
   modalView: {
     alignItems: "center",
-    justifyContent: "center",
-    width: "50%",
-    height: "25%",
-    backgroundColor: "white",
+    backgroundColor: "pink",
     borderRadius: 40,
+    height: "auto",
+    width: "75%",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -34,5 +154,55 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    borderWidth: 5,
+    borderColor: "#bbd7b0",
+    elevation: 1,
+    width: "69%",
+    height: 54,
+    justifyContent: "center",
+    backgroundColor: "#abce9d",
+    marginBottom: 15,
+  },
+  input: {
+    height: 150,
+    width: "90%",
+    margin: 5,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    textAlign: "center",
+  },
+  birthdayInput: {
+    height: 35,
+    color: "black",
+    width: "54%",
+    margin: 5,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    textAlign: "center",
+  },
+  buttonText: {
+    fontSize: 45,
+    fontFamily: "kiddo-font",
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 24,
+    fontFamily: "FatFace-font",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
--- screens/KiddoComments.js
+++ screens/KiddoComments.js
@@ -1,12 +1,105 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  Pressable,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import { useSelector } from "react-redux";
+import AddCommentModal from "../modals/AddCommentModal";
 
 export default function KiddoComments({ route }) {
+  const kiddos = useSelector(state => state);
+  const kiddo = kiddos.find(kiddo => kiddo.id == route.params.kiddoId);
+  const [modalVisible, setModalVisible] = useState(true);
+
+  const renderKiddoNavCards = () => {
+    return kiddos.map(kiddo => {
+      return (
+        <Pressable
+          onPress={() => {
+            console.log("Handle This");
+          }}
+          style={styles.kiddoNavCards}
+          key={kiddo.id}
+        >
+          <Text style={styles.kiddoName}>{kiddo.name}</Text>
+          <Text>{kiddo.age} Years Old</Text>
+          <Text>{kiddo.comments.count || 0} Comments</Text>
+        </Pressable>
+      );
+    });
+  };
+
+  const renderKiddoComments = () => {
+    return kiddo.comments.map(comment => {
+      return <Text key={comment.id}>{comment.content}</Text>;
+    });
+  };
+
   return (
-    <View>
-      <Text>Header: Kiddo Card</Text>
-      <Text>Body </Text>
-      <Text>Navigation </Text>
-    </View>
+    <SafeAreaView style={styles.mainView}>
+      <SafeAreaView style={styles.kiddosHorizontalList}>
+        <ScrollView horizontal={true}>{renderKiddoNavCards()}</ScrollView>
+      </SafeAreaView>
+      <SafeAreaView style={styles.body}>
+        <Text>Body</Text>
+        {renderKiddoComments()}
+      </SafeAreaView>
+      <SafeAreaView style={styles.navigation}>
+        <Text>
+          <Pressable
+            style={styles.addCommentButton}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text>Add Comment</Text>
+          </Pressable>
+        </Text>
+        <AddCommentModal
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        />
+      </SafeAreaView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+  },
+  kiddosHorizontalList: {
+    height: "15%",
+    width: "100%",
+    borderWidth: 1,
+  },
+  kiddoNavCards: {
+    flex: 0,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 5,
+    margin: 10,
+    width: 150,
+    borderWidth: 1,
+  },
+  kiddoName: {
+    color: "red",
+    fontFamily: "kiddo-font",
+    fontSize: 36,
+  },
+  body: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+  },
+  navigation: {
+    height: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+  },
+});
